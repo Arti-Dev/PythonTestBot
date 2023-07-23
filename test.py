c5c2b86788cafcfc2d32d8c1894a9cf3261efa51
Arti-Dev <boomcreeper296@gmail.com>
2023-07-23 16:29:43 -0400

    Add the "challenge"

--- test.py
+++ test.py
@@ -6,34 +6,37 @@ import discord
 import feedparser
 import requests
 import yaml
-from discord import app_commands
+from discord import app_commands, PartialEmoji
 from discord.ext import tasks
 
 from utils import hypixel_date_to_timestamp, save_new_guid, save_default_config
-
-# check if the config file exists
-path = './config.yml'
-if not os.path.isfile(path):
-    save_default_config()
-
-# load config file
-file = open('config.yml', 'r')
-config = yaml.safe_load(file)
-
-token = config['token']
-message_id = config['message-id']
-url = config['url']
-
-file.close()
 
 # check if the last guid file exists
 path = './bestguid.txt'
 if not os.path.isfile(path):
     raise Exception("No bestguid.txt file found in the same directory!")
 
+# check if the config file exists
+path = './config.yml'
+if not os.path.isfile(path):
+    save_default_config()
+
+# load token
+file = open('config.yml', 'r')
+token = yaml.safe_load(file)['token']
+file.close()
+
 
 class Client(discord.Client):
     def __init__(self):
+
+        self.target_message_id = None
+        self.webhook = None
+        self.target_guild = None
+        self.member_role = None
+        self.pass_role = None
+        self.fail_role = None
+        self.challenge_channel: discord.TextChannel = None
 
         # intents
         intents = discord.Intents.default()
@@ -43,21 +46,92 @@ class Client(discord.Client):
 
         super().__init__(intents=intents)
 
-        # load bestguid
+        # load best_guid
         guid_file = open('bestguid.txt', 'r')
         self.best_guid = int(guid_file.readlines()[0])
         guid_file.close()
 
+        # sets up new member dictionary
+        # this is strictly reserved for users who have made NO attempts yet
+        # member ID (string) to task
+        self.new_members = dict()
+
     async def setup_hook(self) -> None:
         self.fetch_hypixel_task.start()
 
+        # load config file
+        configfile = open('config.yml', 'r')
+        config = yaml.safe_load(configfile)
+
+        self.target_message_id = config['message-id']
+        self.webhook = config['url']
+        self.target_guild = await self.fetch_guild(config['guild-id'])
+        self.member_role = self.target_guild.get_role(config['member-role-id'])
+        self.pass_role = self.target_guild.get_role(config['pass-role-id'])
+        self.fail_role = self.target_guild.get_role(config['fail-role-id'])
+        self.challenge_channel = await self.target_guild.fetch_channel(config['challenge-channel-id'])
+        configfile.close()
+
     async def on_ready(self):
         await tree.sync()
-        print(f'Logged in as {client.user}')
-        await client.loop.create_task(list_servers())
+        print(f'Logged in as {self.user}')
+        await self.loop.create_task(list_servers(self))
 
-    async def on_member_join(self, member):
-        await member.send("Welcome to **putt party practice server**!")
+    async def on_member_join(self, member: discord.Member):
+        # only track users in the target server
+        if member.guild.id == self.target_guild.id:
+            print(f"{member.name} with id {member.id} joined!")
+            task = self.loop.create_task(delay(
+                remove_member_from_new_members(self, member),
+                60))
+
+            self.new_members[member.id] = task
+
+    async def on_member_remove(self, member: discord.Member):
+        if member.id in self.new_members:
+            self.new_members[member.id].cancel()
+            del self.new_members[member.id]
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if payload.message_id == self.target_message_id and payload.event_type == 'REACTION_ADD':
+            emoji = payload.emoji
+            member = payload.member
+
+            if emoji.is_unicode_emoji() and ord(emoji.name) == 127881:
+                if member.id in self.new_members:
+                    # there is no need for this task anymore
+                    self.new_members[member.id].cancel()
+
+                    del self.new_members[member.id]
+                    await member.add_roles(self.pass_role, self.member_role,
+                                           reason="Passed the entry challenge as a new member")
+                    await member.send("**Welcome to the Pit Community Discord Server!**\n"
+                                      "Congratulations on passing the #read-me challenge first try!\n"
+                                      "You have been given the cosmetic \"Smartypants\" role as a bonus!\n"
+                                      "**You may now access the rest of the server!**")
+                    print(f"{member.name} PASSED the challenge as a NEW user.")
+                else:
+                    # grant member role even though carl-bot will likely do it
+                    print(f"{member.name} PASSED the challenge as a REGULAR user.")
+                    await member.add_roles(self.member_role, reason="Passed the entry challenge")
+            else:
+                if member.id in self.new_members:
+                    # remove from dictionary, but do not cancel the task
+                    del self.new_members[member.id]
+                    await member.add_roles(self.fail_role, reason="Failed the entry challenge")
+                    message = await self.challenge_channel.send(f"{member.mention}, "
+                                                                f"It looks like you did something wrong. **Pay "
+                                                                f"attention**, then try again.")
+                    await message.delete(delay=10)
+                    print(f"{member.name} failed the challenge as a NEW user.\n"
+                          f"They used the {emoji.name} emoji.")
+                else:
+                    message = await self.challenge_channel.send(f"{member.mention}, "
+                                                                f"It looks like you did something wrong. **Pay "
+                                                                f"attention**, then try again.")
+                    await message.delete(delay=10)
+                    print(f"{member.name} failed the challenge as a REGULAR user.\n"
+                          f"They used the {emoji.name} emoji.")
 
     @tasks.loop(seconds=60)
     async def fetch_hypixel_task(self):
@@ -77,7 +151,7 @@ class Client(discord.Client):
         # post only the first thread that is newer than the previous posted thread
         if len(guids) > 0:
             target_guid = guids[0]
-            post_new_thread(rss, guid_to_entry[target_guid])
+            post_new_thread(self, rss, guid_to_entry[target_guid])
             self.best_guid = target_guid
             print(f"New best guid is {self.best_guid}. Writing to disk.")
             if len(guids) > 1:
@@ -93,7 +167,37 @@ async def delay(coro, seconds):
     await coro
 
 
-def post_new_thread(rss, thread_entry):
+async def remove_member_from_new_members(client, member: discord.Member):
+    # If this method is called, it means that the challenge was not solved.
+
+    # Make sure the user doesn't have preexisting roles
+    if client.member_role in member.roles:
+        print(f"{member.name} had a timer ticking, but they already have the Member role.")
+        if member.id in client.new_members:
+            del client.new_members[member.id]
+        return
+
+    # Grant member the member role regardless of what happens
+    await member.add_roles(client.member_role)
+
+    # If the member is still in client.new_members, no attempt was made.
+    if member.id in client.new_members:
+        del client.new_members[member.id]
+        await member.add_roles(client.fail_role)
+        await member.send("**Welcome to the Pit Community Discord Server!**\n"
+                          "You did not make an attempt to complete the #read-me challenge within 15 minutes.\n"
+                          "You have been given the cosmetic \"Dumb\" role as a bonus!\n"
+                          "**You may now access the rest of the server!**")
+    else:
+        # The member made an attempt but did not pass within the time limit
+        await member.send("**Welcome to the Pit Community Discord Server!**\n"
+                          "I noticed that you attempted the #read-me challenge, but you never finished.\n"
+                          "**You may now access the rest of the server!**\n"
+                          "You have been given the cosmetic \"Dumb\" role as a bonus!")
+
+
+
+def post_new_thread(client, rss, thread_entry):
     forum_title = rss.feed.title
     title = thread_entry.title
     link = thread_entry.link
@@ -110,14 +214,14 @@ def post_new_thread(rss, thread_entry):
                  "text": f'Thread by {creator}'
              }}
         ]}
-    r = requests.post(url=url, json=json)
+    r = requests.post(url=client.webhook, json=json)
     if r.status_code != 204:
         print(f"There was an error posting the latest thread. guid: {thread_guid}")
     else:
         print(f"Successfully posted: {forum_title}\n{title}\n{link}\n{creator}\n{timestamp}\nguid: {thread_guid}")
 
 
-async def list_servers():
+async def list_servers(client):
     await client.wait_until_ready()
     print("Current servers:")
     for server in client.guilds:
@@ -126,7 +230,6 @@ async def list_servers():
 
 client = Client()
 tree = app_commands.CommandTree(client)
-generalID = 1112913686626054188
 
 
 @tree.command(name='square',
--- utils.py
+++ utils.py
@@ -15,8 +15,15 @@ def save_new_guid(new_guid):
 
 
 def save_default_config():
+    # todo make this not overwrite existing values
     file = open('config.yml', 'w')
     yaml.dump({'token': 'your mom',
                'message-id': 1119465976149327892,
-               'url': 'https://example.com'}, file)
+               'url': 'https://example.com',
+               'guild-id': 1112913686626054184,
+               'member-role-id': 1132112701661921351,
+               'pass-role-id': 1132727677317562436,
+               'fail-role-id': 1132727585466503228,
+               'challenge-channel-id': 1119465092262666271},
+              file)
     file.close()
